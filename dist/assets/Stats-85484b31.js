c69a962682c45bc4f3078be4219751b2fd5bff2a
Mario Martín <mmartin.mrmg@gmail.com>
2023-08-11 13:46:07 +0200

    dist

--- dist/assets/Stats-85484b31.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,a as e,c as s}from"./index-86e160a3.js";const c={},a={class:"page"};function o(n,r){return e(),s("div",a," stats ")}const f=t(c,[["render",o]]);export{f as default};
--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/flex/',
+  base: 'https://marioby9.github.io/flex',
   plugins: [
     vue(),
   ],
